7da5ffed40c3ce0dc5acbfcfa6f0be6558ec903c
Mark <markjconlon@gmail.com>
2017-04-10 14:04:19 -0400

    added route and controller for random kitten images

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
+
+  get '/kitten/:size' => 'pages#kitten'
 end
